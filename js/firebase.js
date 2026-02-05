56694714b52012eaf67a17fa22a08663ec5fe6b6
Daria <partnership@smartscrap.net>
2026-02-05 09:42:47 +0300

    COMMIT_EDITMSG

--- js/firebase.js
+++ js/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "…",
-    authDomain: "…",
+    apiKey: "AIzaSyBwCT_-Hi_gcL_wIhZoQwGnJ5fGdjt1m7s",
+    authDomain: "report-8e1ff.firebaseapp.com",
     projectId: "report-8e1ff",
-    storageBucket: "…",
-    messagingSenderId: "…",
-    appId: "…",
+    storageBucket: "report-8e1ff.firebasestorage.app",
+    messagingSenderId: "759119356704",
+    appId: "1:759119356704:web:2985510863dcab511eed45"
 };
 
 const app = initializeApp(firebaseConfig);
